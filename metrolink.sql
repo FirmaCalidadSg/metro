bdf22ac698735000d97c60652983c85d33fa72db
Victor Zapata <victor.zapata8069@gmail.com>
2024-11-30 15:54:19 -0500

    export bd sql

--- metrolink.sql
+++ metrolink.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2024 a las 15:03:57
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 30-11-2024 a las 21:53:22
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,216 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `metrolink`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE `ciudad` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `departamento` int(11) NOT NULL,
+  `codigo_postal` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ciudad`
+--
+
+INSERT INTO `ciudad` (`id`, `nombre`, `departamento`, `codigo_postal`) VALUES
+(6, 'Ibagué', 1, '000'),
+(7, 'Cali', 2, '760000'),
+(8, 'Cúcuta', 3, '777000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `danoequipo`
+--
+
+CREATE TABLE `danoequipo` (
+  `id` int(11) NOT NULL,
+  `equipo` int(11) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `danoequipo`
+--
+
+INSERT INTO `danoequipo` (`id`, `equipo`, `descripcion`, `fecha`, `estado`) VALUES
+(1, 1, 'Descripcion E1', '2024-11-29 22:50:00', 'OK');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `definicion`
+--
+
+CREATE TABLE `definicion` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `valor` varchar(100) NOT NULL,
+  `descripcion` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `definicion`
+--
+
+INSERT INTO `definicion` (`id`, `nombre`, `valor`, `descripcion`) VALUES
+(1, 'D1', 'Definicion valor', 'Descripcion D1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `pais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `departamento`
+--
+
+INSERT INTO `departamento` (`id`, `nombre`, `pais`) VALUES
+(1, 'Tolima', 1),
+(2, 'Valle del Cauca', 1),
+(3, 'Norte de Santander', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `modelo` varchar(80) NOT NULL,
+  `estado` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `equipo`
+--
+
+INSERT INTO `equipo` (`id`, `nombre`, `modelo`, `estado`) VALUES
+(1, 'E1', 'EM1', 'OK'),
+(3, 'E2', 'EM2', 'OK');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `linea`
+--
+
+CREATE TABLE `linea` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `proceso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `linea`
+--
+
+INSERT INTO `linea` (`id`, `nombre`, `proceso`) VALUES
+(1, 'Linea 1', 1),
+(2, 'Linea 2', 1),
+(3, 'Linea 3', 3),
+(4, 'Linea 4', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lineaproducto`
+--
+
+CREATE TABLE `lineaproducto` (
+  `id` int(11) NOT NULL,
+  `linea` int(11) NOT NULL,
+  `producto` int(11) NOT NULL,
+  `capacidad_produccion` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `lineaproducto`
+--
+
+INSERT INTO `lineaproducto` (`id`, `linea`, `producto`, `capacidad_produccion`) VALUES
+(1, 1, 1, 0.2),
+(2, 1, 2, 0.22),
+(3, 4, 2, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `codigo` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `pais`
+--
+
+INSERT INTO `pais` (`id`, `nombre`, `codigo`) VALUES
+(1, 'Colombia', '57'),
+(3, 'Venezuela', '58');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proceso`
+--
+
+CREATE TABLE `proceso` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `proceso`
+--
+
+INSERT INTO `proceso` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Proceso 1', 'Proceso D1'),
+(3, 'Proceso 2', 'Desc P2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `codigo` varchar(60) NOT NULL,
+  `descripcion` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `nombre`, `codigo`, `descripcion`) VALUES
+(1, 'Producto 1', '231231564', 'Desc P1'),
+(2, 'Producto 2', '002', 'Desc P2');
 
 -- --------------------------------------------------------
 
@@ -66,11 +276,78 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `usuario`, `credencial`, `rol_id`, `identificacion`, `creacion`) VALUES
 (1, 'ALEXANDER', 'OREJUELA  ARBOLEDA', 'root', '$2y$10$iR/2TjBAgXX/kHXFo5An7u5gtJpm9e5/QlnbBOZwfFMj4zkzlBPQy', 1, 14696620, '2024-11-27 14:58:29'),
 (2, 'ALEXANDER', 'OREJUELA  ARBOLEDA', 'root', '$2y$10$2ntEdxSS/UY4518ec8GYUejHlzaolXBKHieH..LvJYWu1DFu.5S6y', 1, 14696620, '2024-11-27 15:08:49'),
-(3, 'Emilia11', 'Jeiger11', 'jemilia11', '$2y$10$qwUeAjtFo05TwxpI5IK4AOY/BpDu.645ZLik48LTS9KnUTNLhpONm', 3, 1112233, '2024-11-27 15:52:19');
+(5, 'VICTOR', 'ZAPATA OCAMPO', 'VZ', '$2y$10$0FuMCoQToiYTSQDmO0k8JekOBbDu/tKvwJoxIkAbDOOhBOhniFZIe', 1, 4456, '2024-11-29 06:20:00'),
+(6, 'BURAK', 'ZAPATA OCAMPO', 'BZ', '$2y$10$26B.gJcg5ixU0ArYjhc09e1g0g6DgEzSEy83uNEL5wniU8nbM83mS', 1, 4458, '2024-11-29 06:23:16');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `departamento` (`departamento`);
+
+--
+-- Indices de la tabla `danoequipo`
+--
+ALTER TABLE `danoequipo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `equipo` (`equipo`);
+
+--
+-- Indices de la tabla `definicion`
+--
+ALTER TABLE `definicion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pais` (`pais`);
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `linea`
+--
+ALTER TABLE `linea`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `proceso` (`proceso`);
+
+--
+-- Indices de la tabla `lineaproducto`
+--
+ALTER TABLE `lineaproducto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `linea` (`linea`),
+  ADD KEY `producto` (`producto`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `proceso`
+--
+ALTER TABLE `proceso`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `roles`
@@ -89,16 +366,111 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `danoequipo`
+--
+ALTER TABLE `danoequipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `definicion`
+--
+ALTER TABLE `definicion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `linea`
+--
+ALTER TABLE `linea`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `lineaproducto`
+--
+ALTER TABLE `lineaproducto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `proceso`
+--
+ALTER TABLE `proceso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD CONSTRAINT `ciudad_ibfk_1` FOREIGN KEY (`departamento`) REFERENCES `departamento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `danoequipo`
+--
+ALTER TABLE `danoequipo`
+  ADD CONSTRAINT `danoequipo_ibfk_1` FOREIGN KEY (`equipo`) REFERENCES `equipo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD CONSTRAINT `departamento_ibfk_1` FOREIGN KEY (`pais`) REFERENCES `pais` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `linea`
+--
+ALTER TABLE `linea`
+  ADD CONSTRAINT `linea_ibfk_1` FOREIGN KEY (`proceso`) REFERENCES `proceso` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `lineaproducto`
+--
+ALTER TABLE `lineaproducto`
+  ADD CONSTRAINT `lineaproducto_ibfk_1` FOREIGN KEY (`linea`) REFERENCES `linea` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lineaproducto_ibfk_2` FOREIGN KEY (`producto`) REFERENCES `producto` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
